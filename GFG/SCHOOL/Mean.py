8f2e7a41301e88fd64480e4f06cd1e3c99c00b5c
SAIKAT KUMAR SARDAR <153124279+saikatsardar01@users.noreply.github.com>
2024-04-30 12:21:54 +0530

    Update Mean.py

--- GFG/SCHOOL/Mean.py
+++ GFG/SCHOOL/Mean.py
@@ -1,1 +1,10 @@
+# Problem Link :- https://www.geeksforgeeks.org/problems/mean0021/1?page=4&difficulty=School&sortBy=submissions
 
+class Solution:
+    def mean(self, N , A):
+        S = 0
+        k = len(A)
+        for i in A:
+            S = S+i
+        mean = S//k
+        return mean
