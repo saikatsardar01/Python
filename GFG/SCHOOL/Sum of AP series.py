e7c4187be31db3c70c50d521c77dabb766c3b4fb
SAIKAT KUMAR SARDAR <153124279+saikatsardar01@users.noreply.github.com>
2024-04-30 12:56:00 +0530

    Update Sum of AP series.py

--- GFG/SCHOOL/Sum of AP series.py
+++ GFG/SCHOOL/Sum of AP series.py
@@ -1,1 +1,12 @@
+# Problem Link :- https://www.geeksforgeeks.org/problems/sum-of-ap-series4512/1?page=4&difficulty=School&sortBy=submissions
 
+# Breaking the code into smaller parts help to understand the logic easily.
+
+# The formula is  S = n/2(2a+(n-1)d)
+
+class Solution:
+	def sum_of_ap(self, n, a, d):
+	    l = 2*a+(n-1)*d
+	    k = n*l
+	    S = k//2
+	    return S
