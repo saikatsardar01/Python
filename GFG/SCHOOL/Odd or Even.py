d9dc4c7a3d1984656f2ef5ec90b06723e169adb3
SAIKAT KUMAR SARDAR <153124279+saikatsardar01@users.noreply.github.com>
2024-04-30 12:15:08 +0530

    Update Odd or Even.py

--- GFG/SCHOOL/Odd or Even.py
+++ GFG/SCHOOL/Odd or Even.py
@@ -1,1 +1,9 @@
-
+#Problem Link :- https://www.geeksforgeeks.org/problems/odd-or-even3618/1?page=2&difficulty=School&sortBy=submissions
+class Solution:
+    def oddEven (ob,N):
+        if N == 1:
+           return('odd')
+        elif N % 2 == 0: 
+            return('even')
+        else:
+            return('odd')
