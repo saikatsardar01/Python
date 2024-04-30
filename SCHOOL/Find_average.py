7c81df17b2b2e88a30a48d7bb0143e467392e7f1
SAIKAT KUMAR SARDAR <153124279+saikatsardar01@users.noreply.github.com>
2024-04-30 12:01:16 +0530

    Update Find_average.py

--- SCHOOL/Find_average.py
+++ SCHOOL/Find_average.py
@@ -1,1 +1,7 @@
+# Write a Program to find the average of two numbers.
 
+a = int(input('Enter your first number : '))
+b = int(input('Enter your second number : '))
+S = a+b
+avg = S/2
+print('The average of the given number is : ',avg)
