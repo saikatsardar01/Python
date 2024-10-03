3726a150d3a6dbead11218b99ef0c3315bcc7c0d
SAIKAT KUMAR SARDAR <153124279+saikatsardar01@users.noreply.github.com>
2024-10-03 23:18:01 +0530

    Update Circumference of a Circle.py

--- Practice/Circumference of a Circle.py
+++ Practice/Circumference of a Circle.py
@@ -1,1 +1,5 @@
 # Circumference of a Circle
+radius = float(input('Enter the radius of the circle :- '))
+pi = 3.14
+circumference = 2*pi*radius
+print('The Circumference of the circle is :- ', circumference)
