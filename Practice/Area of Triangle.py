9ee96947784910e47b1f717b0e4dc14241d3d4b8
sagario9 <saikatsardar180@gmail.com>
2024-10-04 11:12:05 +0530

    Create Area of Triangle.py

---
+++ Practice/Area of Triangle.py
@@ -0,0 +1,5 @@
+#Area of Triangle
+height = float(input('Enter the height of triangle :- '))
+base = float(input('Enter the base of the triangle :- '))
+area = 0.5*base*height
+print('The area of triangle is:- ', area)
